be6770732371d74ff59c9660678d3d52d81ee341
panwar35 <panwarpriyanshu35@gmail.com>
2026-06-05 14:49:03 +0530

    new commit

--- weather_app/views.py
+++ weather_app/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'weather_app/home.html')
+def location(request):
+    return render(request, 'weather_app/location.html')
