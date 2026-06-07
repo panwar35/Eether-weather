0ce0afb60e90a9ffa1d56b1163ac34d68f55af86
panwar35 <panwarpriyanshu35@gmail.com>
2026-06-07 11:19:46 +0530

    Issue Fixed

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["eether-weather.onrender.com",
+    "localhost",
+    "127.0.0.1"]
 
 
 # Application definition
